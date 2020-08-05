e66da372ceec19f76fc88a2575c5cb336254f1e1
ThreeLKDev <ThreeLKDev@gmail.com>
2020-08-05 23:33:36 +0000

    Added xiv commands, removed needlessly verbose logging messages

--- DoDZero.js
+++ DoDZero.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const { CommandoClient } = require('discord.js-commando');
-const { Structures } = require('discord.js');
+const { Structures, MessageCollector } = require('discord.js');
+const SQliteWrapper = require('./sqlitewrapper.js');
 const loader = require('./loader.js');
 const path = require('path');
 const prefix = process.env.PREFIX;
 const token = process.env.TOKEN;
 const discord_owner_id = process.env.OWNER;
+global.appRoot = require('path').resolve(__dirname);
+global.appResources = global.appRoot + '/res';
 
 Structures.extend('Guild', function(Guild) {
   class MusicGuild extends Guild {
@@ -17,10 +20,10 @@ Structures.extend('Guild', function(Guild) {
         nowPlaying: null,
         songDispatcher: null,
         volume: 1,
-	playlist: null,
-	playlistModified: false,
-	editSelection : null,
-	lastNowPlayingEmbed: null
+        playlist: null,
+        playlistModified: false,
+        editSelection : null,
+        lastNowPlayingEmbed: null
       };
       this.triviaData = {
         isTriviaRunning: false,
@@ -30,7 +33,8 @@ Structures.extend('Guild', function(Guild) {
       };
       this.channelWatch = {
         text: [],
-	voice: []
+        voice: [],
+        pixiv: []
       };
     }
   }
@@ -48,53 +52,67 @@ client.registry
     ['music', 'Music Command Group'],
     ['gifs', 'Gif Command Group'],
     ['other', 'random types of commands group'],
-    ['guild', 'guild related commands']
+    ['guild', 'guild related commands'],
+    ['xiv', 'FFXIV related commands']
   ])
   .registerDefaultGroups()
   .registerDefaultCommands({
     eval: true,
     prefix: false,
-    commandState: false
+    commandState: true
   })
   .registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.dispatcher.addInhibitor( msg => {
-	console.log(` -> ${msg.content}`);
 	let inhibit = false;
 	if(msg.guild === null) return;
 	const numChannels = msg.guild.channelWatch.text.length;
 	if(numChannels > 0 ) {
-		console.log(` -- -> numChannels > 0`);
 		inhibit = true;
 		for( let i = 0; i < numChannels; i++ ) {
 			if( msg.channel.id == msg.guild.channelWatch.text[i] ) {
-			console.log(` -- -> Channel allowed`);
 				inhibit = false;
 				break;
 			}
 		}
 		if( inhibit ) {
-			console.log(` -- -> Channel disallowed!`);
 			 return '(Not in recognized command channel)';
 		}
 	} else {
-		console.log(` -- -> numChannels <= 0 ?`);
 		if( msg.author.id != discord_owner_id ) {
-			console.log(` -- -> Author NOT owner`);
 			return '(Not in recognized command channel)';
-		} else console.log(` -- -> Author IS owner`);
+		}
 	}
-	console.log(` -- -> End`);
 //	if( inhibit ) return '(Not in recognized command channel)';
 });
 
 client.once('ready', () => {
   console.log('Ready!');
-  client.user.setActivity(`${prefix}help`, {
+  client.user.setActivity(`for ${prefix}help`, {
     type: 'WATCHING',
     url: 'https://github.com/ThreeLKDev/DoDZero'
   });
-  loader.loadAll(client);
+  loader.loadAllSync(client);
+  client.local = {};
+  client.sqlite = new SQliteWrapper();
+  client.guilds.cache.forEach( guild => {
+    console.log('Foreach guild: ' + guild );
+    console.log('Guild has ' + guild.channelWatch.pixiv.length + ' pixiv channel(s)');
+    for(let i = 0; i < guild.channelWatch.pixiv.length; i++ ) {
+      console.log('Pixiv channel #' + i + ' : ');
+      const channel = guild.channels.cache.get( guild.channelWatch.pixiv[i] );
+      if(channel) {
+        console.log('Succeeded');
+        const collector = channel.createMessageCollector( m => !m.author.bot );
+        collector.on('collect', (msg) => {
+          let isPixivUrl = msg.content.match(/(https:\/\/www\.pixiv\.net(?:\/en)?\/artworks\/[0-9]+)(?:\s(\[?(?:\d+(?:\,\s*|\s)?)*\]?))?/);
+          if(isPixivUrl)
+            client.registry.commands.get('pixiv').run(msg, {pixivUrl: isPixivUrl[1], which: isPixivUrl[2] });
+        });
+      } else console.log('Failed.');
+
+    }
+  });
 });
 
 client.on('voiceStateUpdate', async (___, newState) => {
